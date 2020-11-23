f7defac6658ec65bc23ecb4233acc9df4a2ea61d
Nathan Murray <nathanmurray@Nathans-MBP.attlocal.net>
2020-11-23 10:56:53 -0600

    intermediate update

--- app.py
+++ app.py
@@ -29,6 +29,25 @@ def image_test():
     rtn = '<!DOCTYPE html><html><body><img src="%s" /></body></html>' % url
     return rtn
 
+# test with this -> curl --request POST --data '{"test":123, "test2":124}' http://localhost:5000/ground-truth-result/9821
+@app.route('/ground-truth-result/<image_id>', methods=['POST'])
+def ground_truth_result(image_id):
+    if request.method == 'POST':
+        app.logger.info('Saving ground truth for image: %s', image_id)
+        DATABASE_URL = os.environ['DATABASE_URL']
+        json_result = request.data
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = conn.cursor()
+        query = "update nsfw_server.contributed_image set ground_truth_result = %(json_result)s where id = %(image_id)s"
+        cursor.execute(query, {'image_id': image_id, 'json_result': json_result})
+        conn.commit()
+        cursor.close()
+        conn.close()        
+        return 'complete', 200
+    else:
+        return 'bad request', 400
+
+
 @app.after_request
 def after_request_func(response):
     header = response.headers
