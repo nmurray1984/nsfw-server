7cc563d4a0f7a96fd04b6bfc0c7ffd111444b2eb
Nathan Murray <nathanmurray@Nathans-MBP.attlocal.net>
2020-11-28 12:49:40 -0600

    made azure content mod work

--- jobs/fill_azure_content_moderator_results.py
+++ jobs/fill_azure_content_moderator_results.py
@@ -1,47 +1,35 @@
-import urllib, base64, os, http.client
-import urllib.parse
 import psycopg2
 import json
-import http.client, urllib.request, urllib.parse, urllib.error, base64
 import os
+import requests
 
 DATABASE_URL = os.environ['DATABASE_URL']
 CONTENT_MODERATOR_ENDPOINT = os.environ['CONTENT_MODERATOR_ENDPOINT']
-CONTENT_MODERATOR_SUBSCRIPTION_KEY = os.environ.get['CONTENT_MODERATOR_SUBSCRIPTION_KEY']
+CONTENT_MODERATOR_SUBSCRIPTION_KEY = os.environ['CONTENT_MODERATOR_SUBSCRIPTION_KEY']
+url = 'https://southcentralus.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessImage/Evaluate?CacheImage=false'
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
-query = "SELECT id, convert_from(decode(url, 'base64'), 'UTF-8') as url FROM nsfw_server.contributed_image where azure_content_moderator_result is null"
+query = "SELECT id, image_bytes FROM nsfw_server.contributed_image where azure_content_moderator_result is null limit 10000"
 cursor.execute(query)
 query_results = cursor.fetchall()
 
-for (image_id, url) in query_results:
+for (image_id, image_bytes) in query_results:
     headers = {
-        'Content-Type': 'application/json',
+        # Request headers
+        'Content-Type': 'image/jpeg',
         'Ocp-Apim-Subscription-Key': CONTENT_MODERATOR_SUBSCRIPTION_KEY,
     }
 
-    params = urllib.parse.urlencode({
-        'CacheImage': 'true'
-    })
-
-    body = '''{
-        'DataRepresentation': 'URL',
-        'Value': '%s'
-    }''' % url
-
     try:
-        hconn = http.client.HTTPSConnection('southcentralus.api.cognitive.microsoft.com')
-        hconn.request("POST", "/contentmoderator/moderate/v1.0/ProcessImage/Evaluate?%s" % params, body, headers)
-        response = hconn.getresponse()
-        data = response.read()
-        
+        print(image_id, url)
+        response = requests.post(url, headers=headers, data=image_bytes)
         cursor2 = conn.cursor()
         update_sql = 'update nsfw_server.contributed_image set azure_content_moderator_result = %(data)s where id = %(id)s'
-        cursor2.execute(update_sql, {'data': data.decode('utf-8'), 'id': image_id})
+        cursor2.execute(update_sql, {'data': response.text, 'id': image_id})
         conn.commit()
         cursor2.close()
-        print(image_id, url)
+        print('Complete')
     except Exception as e:
         print("Error: {0}".format(e))
 
