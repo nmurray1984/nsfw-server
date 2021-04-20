7b857316c8c8910c07bf7f4e20a432c49f3cbbb1
Nathan Murray <nathan.d.murray@gmail.com>
2021-04-20 05:44:46 -0500

    first commit

--- Procfile
+++ Procfile
@@ -5,3 +5,4 @@ fill_yahoo_nsfw_results: python -u jobs/fill_yahoo_nsfw_results.py
 fill_mobile_nsfw_03: python -u jobs/fill_mobile_nsfw_0.3.py
 load_images_to_disk: python -u jobs/load_images_to_disk.py
 train_model: python -u jobs/train_model.py
+train: python -u jobs/train.py
--- jobs/load_images_to_disk.py
+++ jobs/load_images_to_disk.py
@@ -24,7 +24,6 @@ query = '''
 SELECT
    c.id,
    c.image_bytes,
-   di.image_set,
    di.classification
 FROM
    nsfw_server.dataset_image di
@@ -39,11 +38,11 @@ cursor = conn.cursor()
 cursor.execute(query)
 
 for row in cursor:
-   image_id, image_bytes, image_set, image_classification = row
+   image_id, image_bytes, image_classification = row
 
    image_file_name = str(image_id) + '.jpg'
    cache_file_path = os.path.join(cache_folder_path, image_file_name)
-   sym_folder_path = os.path.join(IMAGE_FILE_PATH, DATASET_NAME, image_set, image_classification)
+   sym_folder_path = os.path.join(IMAGE_FILE_PATH, DATASET_NAME, image_classification)
    sym_file_path = os.path.join(sym_folder_path, image_file_name)
    
    #check if file exists, and if so, don't write it again
